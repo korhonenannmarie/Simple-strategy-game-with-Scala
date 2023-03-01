3d404c2c763947810eb1868df04bd536ef5cf762
Ann-Marie Korhonen-Chamorro <ann-marie.korhonen-chamorro@aalto.fi>
2023-03-01 15:58:21 +0200

    Project started. Character, Monster, Fighter, Rogue, Mage, UI and Game classes / worksheets created. Base variables and methods started to be implemented.
    Game has been started.
    
    Character modified to suit needs better. Monster will now extend character.
    All changes in character will be done in Character, instead of in Game.

--- src/Game.scala
+++ src/Game.scala
@@ -1,3 +1,10 @@
-class Game
+import scala.collection.mutable.Buffer
+class Game:
 
+  private var roundCount: Int = 0
+  private var currentScore: Int = 0
+  private var isOver: Boolean = false
+  private var highScores = Buffer()
+  private var roundIsOver: Boolean = false
+  private var waveIsOver: Boolean = false
 
